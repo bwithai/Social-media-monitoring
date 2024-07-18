fcfcb69d20d65d04d9059bcbb83941e6a15951e9
bwithai <sanaullah41821@gmail.com>
2024-07-18 14:27:27 +0500

    get X user profile info GraphQL

--- utils.py
+++ utils.py
@@ -14,6 +14,28 @@ def get_database_url():
     return url
 
 
+def __login__(url):
+    import os
+    from selenium import webdriver
+
+    # Path to your Chrome profile
+    chrome_profile_path = os.path.expanduser('~/.config/google-chrome/Profile')
+
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"user-data-dir={chrome_profile_path}")
+    options.add_argument("start-maximized")
+
+    # Initialize the WebDriver with the existing profile
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    input("Press Enter to close the browser...")
+    # Close the browser
+    driver.quit()
+
+    # jalalayn, Jalalhaddad
+
+
 def serialize_object_id(dic):
     dic = [
         {**doc, '_id': str(doc['_id'])} for doc in dic
