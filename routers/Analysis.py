b23118759e448c8817aee5ee0741509ca7f5b749
bwithai <sanaullah41821@gmail.com>
2024-08-06 09:28:52 +0500

    get pdf analysis report

--- routers/Analysis.py
+++ routers/Analysis.py
@@ -1,11 +1,31 @@
-from fastapi import APIRouter
-from starlette.responses import JSONResponse
+import os
 
-from database.queries import get_all_hashtags
+from fastapi import APIRouter
+from starlette.responses import JSONResponse, FileResponse
+
+import database
+from database.queries import get_all_hashtags, get_user_by_id
 from schemas import GraphSchema
 from utils import find_severity_score
 
 router = APIRouter(prefix="/analysis", tags=["Analysis"])
+
+
+@router.get('/get-pdf')
+async def get_pdf(user_id: str):
+    user = get_user_by_id(user_id)
+    if user:
+        # Get the path to the 'database' module directory
+        module_path = os.path.dirname(database.__file__)
+
+        # Construct the path to 'pdf_db' directory
+        pdf_db_path = os.path.join(module_path, 'pdf_db')
+
+        return FileResponse(f"{pdf_db_path}/{user_id}.pdf", media_type='application/pdf', filename='file.pdf')
+    return JSONResponse(
+        content={"message": "User not found. Check if MongoDB is running."},
+        status_code=404
+    )
 
 
 @router.get('/graph-data')
